70e0251aff47c3a6f944ace673c33237f95d45fe
e <1289375156@qq.com>
2016-10-12 16:03:57 +0800

    模拟知乎日报的首页

--- pages/ymy/ymy.js
+++ pages/ymy/ymy.js
@@ -1,24 +1,32 @@
 /*ymy.js*/ 
 var app = getApp()
+var utils = require('../../utils/util.js')
 Page({
-    data:{
-        poster:'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
-        name:'此时此刻',
-        author:'许巍',
-        src:'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46'
+    data: {
+        list: [],
+        duration: 2000,
+        indicatorDots: true,
+        autoplay: true,
+        interval: 3000,
+        loading: false,
+        plain: false
     },
-    audioPlay:function(){
-        this.setData({
-            action:{
-                method:'play'
+    onLoad: function () {
+        var that = this
+        wx.request({
+            url: 'http://news-at.zhihu.com/api/4/news/latest',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            success: function (res) {
+                that.setData({
+                    banner: res.data.top_stories,
+                    list: [{ header: '今日热闻' }].concat(res.data.stories),
+                    conTitle:res.data.stories
+                })
             }
         })
-    },
-    audioPause:function(){
-        this.setData({
-            action:{
-                method:'pause'
-            }
-        })
+         this.index = 1
     }
+        
 })
