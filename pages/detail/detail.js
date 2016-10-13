ef0d63002a76fa4dbee0ec836fd7d79b30e4d539
e <1289375156@qq.com>
2016-10-13 16:21:06 +0800

    添加分享的页面

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -2,14 +2,38 @@
 //获取应用实例
 var app = getApp()
 Page({
-    list:[],
+    list:[],   
     onLoad:function(options){
-        
         var that = this
         this.setData({
-           items : options
+           items : options,
+           share:{
+                mode:'aspectFit',
+                src:'../../images/share.png'
+            },
+            thumbup:{
+                mode:'aspectFit',
+                src:'../../images/thumbup.png'
+            },
+            comment:{
+                mode:'aspectFit',
+                src:'../../images/comment.png'
+            },
+            shareto:[{
+                    mode:'aspectFit',
+                    src:'../../images/qq.png'    
+                },{
+                    mode:'aspectFit',
+                    src:'../../images/wx.png'  
+                },{
+                    mode:'aspectFit',
+                    src:'../../images/pengyou.png'  
+                },{
+                    mode:'aspectFit',
+                    src:'../../images/weibo.png'  
+                }],
+            modalHidden:true
         })
-      
     },
     onReady:function(){
     // 页面渲染完成
@@ -20,5 +44,18 @@ Page({
   onUnload:function(){
     // 页面关闭
      wx.navigateBack();
+  },
+  shareNews:function(){
+    // 事件处理
+    this.setData({
+        modalHidden:false
+    })
+  },
+  modalChange:function(){
+    this.setData({
+        modalHidden:true
+    })
   }
+    
+
 })
